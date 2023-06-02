3abfa46f0f39e3e1f420777e78f28a1b4b31fa03
mackeraldev <sabashaw.home@gmail.com>
2023-06-02 10:34:48 +1000

    Componentise inputs

---
+++ src/lib/components/Input.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+    export let maxlength: number = 524288;
+    export let min: string = "";
+    export let max: string = "";
+    export let placeholder: string = "";
+    export let omitBorderStyle: boolean = false;
+    export let value: any = "";
+</script>
+
+<input {maxlength} {min} {max} {placeholder} bind:value={value} on:input class="{$$props.class || ''} w-64 h-10 px-3 placeholder:text-stone-500 text-2xl font-medium text-center bg-zinc-700 {omitBorderStyle ? "outline-none" : "rounded-md outline outline-1 outline-offset-0 outline-sky-600 transition-all duration-75 focus:outline-sky-500 focus:outline-2"}"/>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import Input from "../lib/components/Input.svelte";
 
 	let joinCode: string = "";
 
@@ -20,7 +21,7 @@
 
 <div class="flex flex-col gap-8">
 	<form on:submit|preventDefault={joinRoom} class="flex flex-row w-64 h-10 overflow-clip rounded-md outline outline-1 outline-offset-0 outline-sky-600 transition-all duration-75 focus-within:outline-sky-500 focus-within:outline-2">
-		<input maxlength="6" min="0" bind:value={joinCode} on:input={validateJoinCodeNumerical} placeholder="Enter code" class="flex-grow w-full px-3 placeholder:text-stone-500 text-2xl font-medium text-center bg-zinc-700 outline-none"/>
+		<Input maxlength={6} min=0 bind:value={joinCode} on:input={validateJoinCodeNumerical} placeholder="Enter Code" omitBorderStyle class="flex-grow w-full"></Input>
 		<button type="submit" class="group flex box-content items-center w-10 px-1.5 bg-sky-500 transition-colors duration-200 hover:bg-sky-400">
 			<svg viewBox="0 0 24 24" class="fill-sky-100 transition-colors duration-200 group-hover:fill-stone-100">
 				<path d="M12.1714 10.9998L7.51451 6.34292L8.92872 4.92871L15.9998 11.9998L8.92872 19.0708L7.51451 17.6566L12.1713 12.9998L2.99953 12.9999L2.99951 10.9999L12.1714 10.9998ZM17.9996 18.9997L17.9996 4.99972H19.9996L19.9996 18.9997H17.9996Z"/>
