d9e0840f9b74f57cc44375a6db90df8f62f6df02
mackeraldev <sabashaw.home@gmail.com>
2023-06-14 11:05:26 +1000

    Fix inconsistent colouring

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -29,13 +29,13 @@
         <form on:submit|preventDefault={joinRoom} class="flex flex-row w-48 h-fit overflow-clip rounded-lg outline outline-1 outline-offset-0 outline-zinc-400 transition-all focus-within:outline-violet-500 focus-within:outline-2">
             <Input maxlength={6} min="0" bind:value={joinCode} on:input={validateJoinCodeNumerical} placeholder="Enter Code" omitBorderStyle class="flex-grow w-full h-9" />
             <button type="submit" class="group flex box-content items-center w-8 px-0.5 bg-zinc-100 transition-colors hover:bg-violet-500">
-                <svg viewBox="0 0 24 24" class="fill-zinc-400 transition-colors group-hover:fill-stone-100">
+                <svg viewBox="0 0 24 24" class="fill-zinc-400 transition-colors group-hover:fill-zinc-100">
                     <path d="M12.1714 10.9998L7.51451 6.34292L8.92872 4.92871L15.9998 11.9998L8.92872 19.0708L7.51451 17.6566L12.1713 12.9998L2.99953 12.9999L2.99951 10.9999L12.1714 10.9998ZM17.9996 18.9997L17.9996 4.99972H19.9996L19.9996 18.9997H17.9996Z" />
                 </svg>
             </button>
         </form>
     </div>
-    <div class="block w-px flex-grow bg-stone-300" />
+    <div class="block w-px flex-grow bg-zinc-300" />
     <div class="flex flex-col gap-2 w-64">
         <h1 class="text-center font-bold text-2xl mb-4">Host</h1>
         <div class="flex flex-col">
