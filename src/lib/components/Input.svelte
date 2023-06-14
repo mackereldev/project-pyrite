447548d5e7fde98f9524a22f252fc89d66f95c6a
mackeraldev <sabashaw.home@gmail.com>
2023-06-14 14:46:50 +1000

    Add further input validation

--- src/lib/components/Input.svelte
+++ src/lib/components/Input.svelte
@@ -1,10 +1,13 @@
 <script lang="ts">
+    export let minlength: number = -524288;
     export let maxlength: number = 524288;
     export let min: string = "";
     export let max: string = "";
     export let placeholder: string = "";
+    export let required: boolean = false;
     export let omitBorderStyle: boolean = false;
     export let value: any = "";
+    export let element: HTMLInputElement | null = null;
 </script>
 
-<input {maxlength} {min} {max} {placeholder} bind:value={value} on:input class="{$$props.class || ''} w-64 h-8 px-3 placeholder:text-zinc-300 text-lg font-medium text-center bg-zinc-100 transition-all {omitBorderStyle ? "outline-none" : "rounded-lg outline outline-1 outline-offset-0 outline-zinc-400 focus:outline-violet-500 focus:outline-2"}"/>
+<input {minlength} {maxlength} {min} {max} {placeholder} {required} bind:this={element} bind:value={value} on:input class="{$$props.class || ''} w-64 h-8 px-3 placeholder:text-zinc-300 text-lg font-medium text-center bg-zinc-100 transition-all {omitBorderStyle ? "outline-none" : "rounded-lg outline outline-1 outline-offset-0 outline-zinc-400 focus:outline-violet-500 focus:outline-2"}"/>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,6 +3,8 @@
     import RadioButtonGroup from "../lib/components/RadioButtonGroup.svelte";
     import RadioButton from "../lib/components/RadioButton.svelte";
 
+    const JOIN_CODE_LENGTH = 6;
+    let joinInput: HTMLInputElement;
     let joinCode: string = "";
     let maxLatency = 500;
     let maxClients = 4;
@@ -13,7 +15,8 @@
 
     const joinRoom = () => {
         // Validation
-        validateJoinCodeNumerical(); // Numeric
+        if (!joinInput.validity.valid) return; // Length (range), non-empty (existence)
+        validateJoinCodeNumerical(); // Numeric (type)
 
         console.log(`Joining room: ${joinCode}.`);
     };
@@ -27,7 +30,7 @@
     <div class="flex flex-col gap-2 justify-center">
         <h1 class="text-center font-bold text-2xl mb-4">Join</h1>
         <form on:submit|preventDefault={joinRoom} class="flex flex-row w-48 h-fit overflow-clip rounded-lg outline outline-1 outline-offset-0 outline-zinc-400 transition-all focus-within:outline-violet-500 focus-within:outline-2">
-            <Input maxlength={6} min="0" bind:value={joinCode} on:input={validateJoinCodeNumerical} placeholder="Enter Code" omitBorderStyle class="flex-grow w-full h-9" />
+            <Input minlength={JOIN_CODE_LENGTH} maxlength={JOIN_CODE_LENGTH} min="0" bind:element={joinInput} bind:value={joinCode} on:input={validateJoinCodeNumerical} placeholder="Enter Code" required omitBorderStyle class="flex-grow w-full h-9" />
             <button type="submit" class="group flex box-content items-center w-8 px-0.5 bg-zinc-100 transition-colors hover:bg-violet-500">
                 <svg viewBox="0 0 24 24" class="fill-zinc-400 transition-colors group-hover:fill-zinc-100">
                     <path d="M12.1714 10.9998L7.51451 6.34292L8.92872 4.92871L15.9998 11.9998L8.92872 19.0708L7.51451 17.6566L12.1713 12.9998L2.99953 12.9999L2.99951 10.9999L12.1714 10.9998ZM17.9996 18.9997L17.9996 4.99972H19.9996L19.9996 18.9997H17.9996Z" />
