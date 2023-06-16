06b06ba32ec734a1808b12f30a91c0f7302fe430
mackeraldev <sabashaw.home@gmail.com>
2023-06-16 11:04:45 +1000

    Improve outline styling

--- src/lib/components/Input.svelte
+++ src/lib/components/Input.svelte
@@ -5,9 +5,9 @@
     export let max: string = "";
     export let placeholder: string = "";
     export let required: boolean = false;
-    export let omitBorderStyle: boolean = false;
+    export let omitRingStyle: boolean = false;
     export let value: any = "";
     export let element: HTMLInputElement | null = null;
 </script>
 
-<input {minlength} {maxlength} {min} {max} {placeholder} {required} bind:this={element} bind:value={value} on:input class="{$$props.class || ''} w-64 h-8 px-3 placeholder:text-zinc-300 text-lg font-medium text-center bg-zinc-100 transition-all {omitBorderStyle ? "outline-none" : "rounded-lg outline outline-1 outline-offset-0 outline-zinc-400 focus:outline-violet-500 focus:outline-2"}"/>
+<input {minlength} {maxlength} {min} {max} {placeholder} {required} bind:this={element} bind:value={value} on:input class="{$$props.class || ''} w-64 h-8 px-3 placeholder:text-zinc-300 text-lg font-medium text-center bg-zinc-100 transition-all {!omitRingStyle && "ring-input"}"/>
--- src/lib/components/RadioButtonGroup.svelte
+++ src/lib/components/RadioButtonGroup.svelte
@@ -1,3 +1,3 @@
-<div class="{$$props.class || ''} flex gap-px min-w-[8rem] h-9 overflow-clip rounded-lg bg-zinc-200 outline outline-1 outline-zinc-400">
+<div class="{$$props.class || ''} flex gap-px min-w-[8rem] h-9 overflow-clip rounded-lg bg-zinc-200 ring-1 ring-zinc-400">
     <slot />
 </div>
