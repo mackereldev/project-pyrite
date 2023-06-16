ddfbc69b559e33aa123dea5c0af743622b9f064a
mackeraldev <sabashaw.home@gmail.com>
2023-06-16 12:14:48 +1000

    Add additional page status support

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -3,22 +3,21 @@
     import { page } from "$app/stores";
     import { goto } from "$app/navigation";
 
-    let statusDesc: string;
-    switch ($page.status) {
-        case 404:
-            statusDesc = "Page not found";
-            break;
-        default:
-            statusDesc = "Unknown error";
-            break;
-    }
+    $: message = statusMessages[$page.status as keyof typeof statusMessages];
+
+    const statusMessages = {
+        404: "Page not found",
+        401: "Unauthorized",
+        500: "Internal error",
+        502: "Bad gateway"
+    };
 </script>
 
 <div class="flex flex-col items-center justify-center gap-12">
     <div class="flex flex-row items-center justify-center gap-4">
         <h1 class="text-5xl font-light">{$page.status}</h1>
         <div class="w-px h-12 flex-grow bg-zinc-300" />
-        <span class="text-lg">{statusDesc}</span>
+        <span class="text-lg">{message}</span>
     </div>
     <Button on:click={() => goto("/")} class="absolute top-[60%]">Return Home</Button>
 </div>
