051de0237d893f268edeede1d02ed91452ade8b2
Saba Shaw <66781907+mackereldev@users.noreply.github.com>
2023-06-25 21:49:31 +1000

    Ensure Presence events are subscribed before entering channel

--- src/routes/play/[code]/+page.svelte
+++ src/routes/play/[code]/+page.svelte
@@ -27,23 +27,23 @@
     
         channel = realtime.channels.get(code);
     
-        channel.presence.subscribe("present", (ctx) => {
+        await channel.presence.subscribe("present", (ctx) => {
             console.log(`${ctx.clientId} is already here.`);
             players = [...players, ctx.clientId];
         });
         
-        channel.presence.subscribe("enter", (ctx) => {
+        await channel.presence.subscribe("enter", (ctx) => {
             console.log(`${ctx.clientId} joined the room.`);
             players = [...players, ctx.clientId];
         });
         
-        channel.presence.subscribe("leave", (ctx) => {
+        await channel.presence.subscribe("leave", (ctx) => {
             console.log(`${ctx.clientId} left the room.`);
             players.splice(players.indexOf(ctx.clientId), 1);
             players = [...players];
         });
         
-        channel.presence.enter();
+        await channel.presence.enter();
     })
 </script>
 
