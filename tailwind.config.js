4e381950e7f6d2bc55f96afcb62a3f02b5150323
Saba Shaw <66781907+mackereldev@users.noreply.github.com>
2023-07-04 13:14:21 +1000

    Fix default transition duration

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,17 @@ export default {
         fontFamily: {
             sans: ["Inter", "ui-sans-serif", "system-ui", "-apple-system", "BlinkMacSystemFont", "Segoe UI", "Roboto", "Helvetica Neue", "Arial", "Noto Sans", "sans-serif", "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"],
         },
+        transitionDuration: {
+            DEFAULT: "100ms",
+            75: "75ms",
+            100: "100ms",
+            150: "150ms",
+            200: "200ms",
+            300: "300ms",
+            500: "500ms",
+            700: "700ms",
+            1000: "1000ms",
+        },
         extend: {},
     },
     plugins: [],
