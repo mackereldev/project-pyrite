4cba00de6eb18ed99dd0359b856210b1b2c78004
Saba Shaw <66781907+mackereldev@users.noreply.github.com>
2023-07-29 21:54:01 +1000

    Include server directories in Vite watch config

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
         port: 5173,
         strictPort: true,
         watch: {
-            ignored: ["build", "**/server/**"],
+            ignored: ["build"],
         },
     },
     build: {
