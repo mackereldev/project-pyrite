7e7b9e17779c62230e814a9d6e2a80811c30b419
Saba Shaw <66781907+mackereldev@users.noreply.github.com>
2023-07-30 23:33:22 +1000

    Revert "Include server directories in Vite watch config"
    This reverts commit 4cba00de6eb18ed99dd0359b856210b1b2c78004.

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
         port: 5173,
         strictPort: true,
         watch: {
-            ignored: ["build"],
+            ignored: ["build", "**/server/**"],
         },
     },
     build: {
