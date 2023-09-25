d16bcd410148f49ad66a1e0e9e649f74646a1f4a
Saba Shaw <66781907+mackereldev@users.noreply.github.com>
2023-09-25 22:37:55 +1000

    Tweak ChatItem and ChatTabContent

--- src/lib/components/ChatItem.svelte
+++ src/lib/components/ChatItem.svelte
@@ -11,13 +11,13 @@
         colour = "text-violet-500";
         author = message.author || "invalid";
     } else if (message.type === "system") {
-        colour = "text-green-500";
+        colour = "text-sky-500";
         author = "SYSTEM";
     }
 </script>
 
-<div class={`font-mono${unreadIndicator ? " unread-msg-shadow" : ""}`}>
+<div class="font-mono{unreadIndicator ? " unread-msg-shadow" : ""}">
     <span class="text-zinc-400">{message.getRelativeTime(relativeStartTime)}</span>
-    <span class={colour}>[{author}]</span>
-    <span class={`whitespace-pre-wrap${message.isError ? " text-red-400" : ""}`}>{message.text}</span>
+    <span class="font-bold {colour}">[{author}]</span>
+    <span class="whitespace-pre-wrap{message.isError ? " text-red-400" : ""}">{message.text}</span>
 </div>
--- src/lib/components/ChatTabContent.svelte
+++ src/lib/components/ChatTabContent.svelte
@@ -104,11 +104,11 @@
     </div>
     <div class="flex basis-80 flex-col">
         <div class="flex flex-1 flex-col overflow-clip border-b-2 border-zinc-300 p-5">
-            <span class="border-b-2 border-zinc-300 pb-2 text-2xl">Clients</span>
+            <span class="border-b-2 border-zinc-300 pb-2 text-2xl">Members</span>
             <div class="flex flex-col overflow-y-scroll">
-                {#each $clients as peer}
+                {#each $clients as client}
                     <div class="flex p-4">
-                        {#if peer.isLeader}
+                        {#if client.isLeader}
                             <span class="mr-2">
                                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="h-6 w-6 fill-amber-400 stroke-amber-400 stroke-2">
                                     <path d="M4 19H20M11.2929 5.70711L8.70711 8.2929C8.31658 8.68342 7.68342 8.68342 7.29289 8.2929L5.70711 6.70711C5.07714 6.07714 4 6.52331 4 7.41422V15C4 15.5523 4.44772 16 5 16H19C19.5523 16 20 15.5523 20 15V7.41421C20 6.52331 18.9229 6.07714 18.2929 6.70711L16.7071 8.2929C16.3166 8.68342 15.6834 8.68342 15.2929 8.2929L12.7071 5.70711C12.3166 5.31658 11.6834 5.31658 11.2929 5.70711Z" stroke-linecap="round" stroke-linejoin="round" />
@@ -122,7 +122,7 @@
                                 </svg>
                             </span>
                         {/if}
-                        <span class="font-bold">{peer.clientId}</span>
+                        <span class="font-bold">{client.clientId}</span>
                     </div>
                 {/each}
             </div>
