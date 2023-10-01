dd678e8a8beb94a160ab522d409384adcd96851d
Saba Shaw <66781907+mackereldev@users.noreply.github.com>
2023-10-02 10:06:57 +1100

    Setup the target server address

--- src/lib/classes/Stores.ts
+++ src/lib/classes/Stores.ts
@@ -4,7 +4,7 @@ import type ToastContainer from "$lib/components/ToastContainer.svelte";
 import type PreferencesModal from "$lib/components/Modal/PreferencesModal.svelte";
 import type ExportModal from "$lib/components/Modal/ExportModal.svelte";
 
-export const clientStore = writable<Colyseus.Client>(new Colyseus.Client("ws://localhost:2567"));
+export const clientStore = writable<Colyseus.Client>(new Colyseus.Client("https://pyrite-backend.mackerel.dev"));
 export const toastContainerStore = writable<ToastContainer>();
 export const preferencesModalStore = writable<PreferencesModal>();
 export const exportModalStore = writable<ExportModal>();
