bfa19bf0f3bb02d3b8a0ae23c9c756ed328ea34c
Saba Shaw <66781907+mackereldev@users.noreply.github.com>
2023-10-04 09:27:02 +1100

    Stop tracking .netlify directory

--- .netlify/server/nodes/0.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 0;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/pages/_layout.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/0.fda03a46.js","_app/immutable/chunks/scheduler.cb1169d2.js","_app/immutable/chunks/index.55e9778b.js","_app/immutable/chunks/index.358416fc.js","_app/immutable/chunks/index.daab3450.js"];
-export const stylesheets = ["_app/immutable/assets/0.876a4c67.css"];
-export const fonts = [];
--- .netlify/server/nodes/2.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 2;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/pages/_page.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/2.33e9fe75.js","_app/immutable/chunks/scheduler.cb1169d2.js","_app/immutable/chunks/index.55e9778b.js","_app/immutable/chunks/index.358416fc.js","_app/immutable/chunks/index.daab3450.js","_app/immutable/chunks/navigation.eeea3a63.js","_app/immutable/chunks/singletons.258fba34.js"];
-export const stylesheets = ["_app/immutable/assets/2.071c4a26.css"];
-export const fonts = [];
